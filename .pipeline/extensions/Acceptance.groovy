e2a4089f25fa69c087cd65e856edad6c38657a4e
D064809 <tom.bendrath@sap.com>
2021-02-11 17:46:15 +0100

    .

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -1,7 +1,7 @@
 void call(Map params) {
     echo "Start - Extension for stage: ${params.stageName}"
     echo "Current stage config: ${params.config}"
-    params.originalStage()  // Execute original stage
+    // params.originalStage()  // Execute original stage
 
     dockerExecute(dockerImage: 'node:lts-stretch'){
         sh """
@@ -11,13 +11,17 @@ void call(Map params) {
         """
     }
 
+    echo "Finished UIVeri5Test preparation"
+
     // Setup the environment to start the application with CAP server
     // and execute UiVeri5 system tests
     uiVeri5ExecuteTests script: this,
-    runOptions: ["./app/admin/webapp/test/uiveri5/conf.js"]
+    // runOptions: ["./app/admin/webapp/test/uiveri5/conf.js"]
     // installCommand: "npm install @sap/cds --global --quiet && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @sap/cds-dk --global --quiet --force && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @ui5/uiveri5 --global --quiet && npm install --force && (/home/node/.npm-global/lib/node_modules/@sap/cds/bin/cds.js watch > cds.log 2>&1 &)"
-    // runCommand: "sleep 10 && cd /home/jenkins/agent/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/lib/node_modules/@ui5/uiveri5/bin/uiveri5 --seleniumAddress='http://selenium:4444/wd/hub' --baseUrl='http://npm:4004/fiori.html#manage-books' -v"
+    runCommand: "sleep 10 && cd /home/jenkins/agent/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/lib/node_modules/@ui5/uiveri5/bin/uiveri5 --seleniumAddress='http://selenium:4444/wd/hub' --baseUrl='http://npm:4004/fiori.html#manage-books' -v"
     
+    echo "Finished UIVeri5Test exeuction"
+
     // HTML Publisher plugin
     // Publish HTML reports
     // Publish Test Report for UIveri5 on Jenkins  
