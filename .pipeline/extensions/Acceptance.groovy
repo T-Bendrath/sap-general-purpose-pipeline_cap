608bcfe5c286a33dbedba65863ce03990487eaba
D064809 <tom.bendrath@sap.com>
2021-02-11 19:17:48 +0100

    .

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -17,7 +17,7 @@ void call(Map params) {
     // and execute UiVeri5 system tests
     uiVeri5ExecuteTests script: this,
     // installCommand: "npm install @sap/cds --global --quiet && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @sap/cds-dk --global --quiet --force && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @ui5/uiveri5 --global --quiet && npm install --force && (/home/node/.npm-global/lib/node_modules/@sap/cds/bin/cds.js watch > cds.log 2>&1 &)"
-    runCommand: "sleep 10 && /home/node/.npm-global/bin/uiveri5 --seleniumAddress='http://selenium:4444/wd/hub' --baseUrl='http://npm:4004/fiori.html#manage-books' -v"
+    runCommand: "sleep 10 && cd /var/jenkins_home/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/lib/node_modules/@ui5/uiveri5/bin/uiveri5"
     
     echo "Finished UIVeri5Test exeuction"
 
