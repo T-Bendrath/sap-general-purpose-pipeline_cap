7fb90b3225e06be284887cd41af25caece53ac89
D064809 <tom.bendrath@sap.com>
2021-02-11 22:55:55 +0100

    .

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -18,7 +18,7 @@ void call(Map params) {
     uiVeri5ExecuteTests script: this,
     // runOptions: ["/var/jenkins_home/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/conf.js"]
     // installCommand: "npm install @sap/cds --global --quiet && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @sap/cds-dk --global --quiet --force && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @ui5/uiveri5 --global --quiet && npm install --force && (/home/node/.npm-global/lib/node_modules/@sap/cds/bin/cds.js watch > cds.log 2>&1 &)"
-    runCommand: "cd /var/jenkins_home/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/lib/node_modules/@ui5/uiveri5/bin/uiveri5"
+    runCommand: "cd /home/jenkins/agent/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/bin/uiveri5"
     
     echo "Finished UIVeri5Test exeuction"
 
