8d0e5b836052dfe10ad76199d296a7136475132b
D064809 <tom.bendrath@sap.com>
2021-02-11 11:41:55 +0100

    UIVeri5Test script improvements

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -1,9 +1,15 @@
 void call(Map params) {
+    echo "Start - Extension for stage: ${params.stageName}"
+    echo "Current stage config: ${params.config}"
+    params.originalStage()  // Execute original stage
+
     // Setup the environment to start the application with CAP server
     // and execute UiVeri5 system tests
     uiVeri5ExecuteTests script: this,
-    installCommand: "npm install @sap/cds --global --quiet && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @sap/cds-dk --global --quiet --force && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @ui5/uiveri5 --global --quiet && npm install --force && (/home/node/.npm-global/lib/node_modules/@sap/cds/bin/cds.js watch > cds.log 2>&1 &)",
-    runCommand: "sleep 10 && cd /var/jenkins_home/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/lib/node_modules/@ui5/uiveri5/bin/uiveri5 --seleniumAddress='http://selenium:4444/wd/hub' --baseUrl='http://npm:4004/fiori.html#manage-books' -v"
+    installCommand: "cds watch",
+    // installCommand: "npm install @sap/cds --global --quiet && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @sap/cds-dk --global --quiet --force && NPM_CONFIG_PREFIX=/home/node/.npm-global npm install @ui5/uiveri5 --global --quiet && npm install --force && (/home/node/.npm-global/lib/node_modules/@sap/cds/bin/cds.js watch > cds.log 2>&1 &)"
+    runOptions: ["--seleniumAddress=http://localhost:4444/wd/hub", "./app/admin/webapp/test/uiveri5/conf.js"]
+    // runCommand: "sleep 10 && cd /var/jenkins_home/workspace/${JOB_NAME}/app/admin/webapp/test/uiveri5/ && /home/node/.npm-global/lib/node_modules/@ui5/uiveri5/bin/uiveri5 --seleniumAddress='http://selenium:4444/wd/hub' --baseUrl='http://npm:4004/fiori.html#manage-books' -v"
     
     // HTML Publisher plugin
     // Publish HTML reports
@@ -16,5 +22,7 @@ void call(Map params) {
         reportFiles: "report.html",
         reportName: "UIVeri5 Test Report"
     ]
+
+    echo "End - Extension for stage: ${params.stageName}"
 }
 return this
