daa7eaf8a27517c94025a0a0296e3eeb3b2552aa
D064809 <tom.bendrath@sap.com>
2021-02-12 17:46:34 +0100

    .

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -2,7 +2,7 @@ const cds = require('@sap/cds')
 
 module.exports = cds.service.impl (async function() {
   const {Books} = cds.entities
-  const {ID} = await SELECT.one.from(Books).columns('max(ID) as ID')
+  const {ID} = await SELECT.one(Books).columns('max(ID) as ID')
   let newID = ID - ID % 100 + 100
   this.before ('NEW','Books', req => req.data.ID = ++newID)
 })
