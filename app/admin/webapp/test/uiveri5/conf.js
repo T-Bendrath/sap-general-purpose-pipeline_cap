0119724cba21a1906534713adbc1f113273697b6
D064809 <tom.bendrath@sap.com>
2021-02-15 15:17:21 +0100

    .

---
+++ app/admin/webapp/test/uiveri5/conf.js
@@ -0,0 +1,11 @@
+'use strict'
+const path = require('path');
+const specs = path.relative(process.cwd(), path.join(__dirname, './tests/*.spec.js'));
+// const specs = path.relative(process.cwd(), path.join(__dirname, '/app/admin/webapp/test/uiveri5/tests/*.spec.js'));
+
+exports.config = {
+  profile: "integration",
+  // baseUrl: "https://cf-dts-integration-de-opensap-capire-bookshop-approuter.cfapps.eu10.hana.ondemand.com/app/fiori.html#manage-books",
+  baseUrl: "http://localhost:4004/fiori.html#manage-books",
+  specs: specs
+};
