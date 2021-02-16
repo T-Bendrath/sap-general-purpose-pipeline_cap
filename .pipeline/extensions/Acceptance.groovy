9c235acb77150234b151cd679b8f5cd6ad0723dd
D064809 <tom.bendrath@sap.com>
2021-02-16 16:43:48 +0100

    .

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -3,14 +3,15 @@ void call(Map params) {
     echo "Current stage config: ${params.config}"
 
     try {
+        // Setup the environment to start the application with CAP server and execute UIVeri5 system tests
+        
         git credentialsId: 'githubCredentialsId',
             url: 'git@github.com:T-Bendrath/openSAP-CAP-Pipeline.git',
             branch: 'main'
 
-        // Setup the environment to start the application with CAP server and execute UIVeri5 system tests
         uiVeri5ExecuteTests script: this
-        echo "Finished UIVeri5Test exeuction"
-
+        
+        echo "Finished UIVeri5Test execution"
     } finally {
 
         // Publish Test Report for UIVeri5 on Jenkins
