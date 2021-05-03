ba8a6f74902738d7285cf0e33af43ad816b3d575
D064809 <tom.bendrath@sap.com>
2021-05-03 11:23:57 +0200

    Use localhost url by default for uiveri5

--- app/admin/webapp/test/uiveri5/conf.js
+++ app/admin/webapp/test/uiveri5/conf.js
@@ -3,10 +3,10 @@ const path = require('path');
 const specs = path.relative(process.cwd(), path.join(__dirname, './tests/*.spec.js'));
 
 // Local testing
-// const baseUrl = "http://localhost:4004/fiori.html#manage-books";
+const baseUrl = "http://localhost:4004/fiori.html#manage-books";
 
 // Remote testing
-const baseUrl = "https://<YOUR_DEPLOYED_APPROUTER_URL>.cfapps.eu10.hana.ondemand.com/app/fiori.html#manage-books";
+// const baseUrl = "https://<YOUR_DEPLOYED_APPROUTER_URL>.cfapps.eu10.hana.ondemand.com/app/fiori.html#manage-books";
 
 exports.config = {
   profile: "integration",
